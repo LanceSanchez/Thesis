9262f16250bdc1c7718b54cae1f22de6748c0b78
LanceSanchez <lancesanchez16@gmail.com>
2019-10-16 23:20:42 +0800

    werk it

--- test.sh
+++ test.sh
@@ -4,10 +4,6 @@ latest=$(ls -t Thesis/snapshot/*.caffemodel | head -n 1)
 if test -z $latest; then
 	exit 1
 fi
-<<<<<<< HEAD
 /content/caffe/build/tools/caffe train -solver="Thesis/solver_test.prototxt" \
-=======
-/content/caffe/build/tools/caffe train -solver="solver_test.prototxt" \
->>>>>>> f1aaee4b34befd2b2bd56a270b81e6c49fd74651
 --weights=$latest \
 -gpu 0
--- train.sh
+++ train.sh
@@ -5,13 +5,8 @@ if ! test -f Thesis/example/MobileNetSSD_train.prototxt ;then
         exit 1
 fi
 mkdir -p snapshot
-<<<<<<< HEAD
 /content/caffe/build/tools/caffe train -solver="Thesis/solver_train.prototxt" \
 -weights="Thesis/mobilenet_iter_73000.caffemodel" \
-=======
-/content/caffe/build/tools/caffe train -solver="solver_train.prototxt" \
--weights="mobilenet_iter_73000.caffemodel" \
->>>>>>> f1aaee4b34befd2b2bd56a270b81e6c49fd74651
 -gpu 0
 
 # -snapshot  snapshot/mobilenet_iter_106000.solverstate \
